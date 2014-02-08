8296923bde8bc28317c0836b577b0524571796db
yajnesh <yajnesh.t@gmail.com>
2014-02-08 17:07:48 +0530

    failed in iplementing #hashtag as list item, reverting back to normal textview

--- Missile/src/com/matrix/missile/controller/adapter/ViewMissileAdapter.java
+++ Missile/src/com/matrix/missile/controller/adapter/ViewMissileAdapter.java
@@ -1,15 +1,13 @@
 package com.matrix.missile.controller.adapter;
 
-import android.app.Activity;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.matrix.missile.R;
-import com.matrix.missile.controller.tag.TagController;
 import com.matrix.missile.model.Missile;
-import com.matrix.missile.view.customviews.component.LinkEnabledTextView;
+import com.matrix.missile.view.customviews.CustomeTextView;
 
 public class ViewMissileAdapter extends SupportArrayAdapter<Missile> {
 
@@ -25,7 +23,7 @@ public class ViewMissileAdapter extends SupportArrayAdapter<Missile> {
 					false);
 			holder = new ViewHolder();
 			holder.title = (TextView) convertView.findViewById(R.id.tvTitle);
-			holder.message = (LinkEnabledTextView) convertView
+			holder.message = (CustomeTextView) convertView
 					.findViewById(R.id.tvMessage);
 			convertView.setTag(holder);
 		} else
@@ -33,14 +31,13 @@ public class ViewMissileAdapter extends SupportArrayAdapter<Missile> {
 
 		holder.title.setText(getItem(position).getTitle());
 		holder.message.setText(getItem(position).getMessage());
-		holder.message.setOnTextLinkClickListener(new TagController(
-				(Activity) getContext()));
+		
 		return convertView;
 	}
 
 	class ViewHolder {
 		TextView title;
-		LinkEnabledTextView message;
+		CustomeTextView message;
 	}
 
 }
