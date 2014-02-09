5337ffbb509dcb687cb84293fcef01fb2f08f7d5
yajnesh <yajnesh.t@gmail.com>
2014-02-09 08:35:03 +0530

    included about page

---
+++ Missile/src/com/matrix/missile/view/activity/AboutAvtivity.java
@@ -0,0 +1,52 @@
+package com.matrix.missile.view.activity;
+
+import com.matrix.missile.R;
+import com.matrix.missile.R.id;
+import com.matrix.missile.R.layout;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.support.v4.app.NavUtils;
+
+public class AboutAvtivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about_avtivity_layout);
+		// Show the Up button in the action bar.
+		setupActionBar();
+	((WebView)findViewById(R.id.wvAbout)).loadUrl("file:///android_asset/about.html");	
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}.
+	 */
+	private void setupActionBar() {
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+	}
+
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
