3ca8635e19fc7572e10b6fab1cf769691cab323f
Vishal Vijay <0vishalvijay0@gmail.com>
2014-02-09 08:43:17 +0530

    comment v1 ui implimentation

--- Missile/src/com/matrix/missile/controller/adapter/CommentAdapter.java
+++ Missile/src/com/matrix/missile/controller/adapter/CommentAdapter.java
@@ -1,12 +1,12 @@
 package com.matrix.missile.controller.adapter;
 
-import com.matrix.missile.R;
-import com.matrix.missile.model.Comment;
-
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.matrix.missile.R;
+import com.matrix.missile.model.Comment;
 
 public class CommentAdapter extends SupportArrayAdapter<Comment> {
 
